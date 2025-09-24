9019bc88df14c3bd3840de5724b416f2459c0f2b
felixggj <me@felix-gomez.com>
2025-09-24 15:34:27 +0200

    separate docker files into fe and be

--- start-backend.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Starting FastAPI Backend Service"
-cd /app
-python -m uvicorn app.api.main:app --host 0.0.0.0 --port ${PORT:-8000}
--- start-frontend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Starting Next.js Frontend Service"
-echo "API URL: $NEXT_PUBLIC_API_URL"
-cd /app/dashboard
-npm start -- --port ${PORT:-3000} --hostname 0.0.0.0
